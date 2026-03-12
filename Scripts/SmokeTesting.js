8b3f95b86a1613f8c513ed2858baf94a55b4c92d
kingsley osemen mutana <kingsose.jr@gmail.com>
2026-03-12 13:11:34 +0100

    new commit

--- Scripts/SmokeTesting.js
+++ Scripts/SmokeTesting.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { sleep, check, group } from 'k6';
-//import {htmlReport} from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import {htmlReport} from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 
 const BASE_URL = __ENV.BASE_URL || 'https://ternny.com';
 
